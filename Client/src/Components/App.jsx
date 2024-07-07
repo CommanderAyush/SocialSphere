cb457f40416afd4c8883162422c1d094755595be
AYUSH <agrawalayush791@gmail.com>
2024-07-08 02:53:23 +0530

    deployment2

--- Client/src/Components/App.jsx
+++ Client/src/Components/App.jsx
@@ -3,7 +3,7 @@ import Register from "./Register";
 import Homepage from "./HomePage";
 import axios from "axios";
 
-axios.defaults.baseURL="http://localhost:3000";
+axios.defaults.baseURL="https://socialsphere-01po.onrender.com";
 axios.defaults.withCredentials=true;
 export const context=React.createContext([]);
 function App()
